a242707aef2339563aa55bd3a378771d193a6a4b
Vandana Pai <vandanapai01@gmail.com>
2016-07-16 14:07:11 -0700

    enter address and display congress members for district

--- public/js/main.js
+++ public/js/main.js
@@ -14,11 +14,29 @@ $(document).ready(function() {
 			success: function(results) {
 				var legislators = results;
 				console.log(legislators);
+				$('#userDistrictNum').html(legislators.results[0].district);
+
+				for(var i = 0; i < legislators.results.length; i++) {
+
+					var currLegislator = legislators.results[i];
+
+					$('#membersCongressList').append("<div id=" + currLegislator.title + " " + "data-phone=" + currLegislator.phone + " " + "data-userID=" + currLegislator.bioguide_id + ">" + "<h3 class='legislator-name'>" + currLegislator.first_name + " " + currLegislator.last_name + "</h3></div>");
+
+					}
+
+				$('#congressResultsPrompt').html("Select congress member to call");
 			}
 		});
 	};
+	
+	$('.legislator-name').on("click", function () {
+		console.log('legislator clicked');
+		callLegislator();
+	});
 
+	function callLegislator(phone) {
 
+	}
 
 
 });
