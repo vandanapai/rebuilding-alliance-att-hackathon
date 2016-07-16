82a9bf52831706730c18bd68efdba7fb4e6b8f5b
Ariel Jakobovits <arieljake@gmail.com>
2016-07-16 15:10:39 -0700

    debug node version, remove favicon

--- app.js
+++ app.js
@@ -60,7 +60,6 @@
 var express = require('express');
 var	fs = require('fs');
 var	https = require('https');
-var	favicon = require('static-favicon');
 var	bodyParser = require('body-parser');
 var compression = require('compression');
 var cors = require('cors');
@@ -163,7 +162,6 @@ var app = express();
 
 app.use(cors());
 app.use(compression());
-app.use(favicon());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/', express.static(__dirname + '/public'));
